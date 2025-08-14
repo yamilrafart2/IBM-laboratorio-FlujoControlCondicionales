d223f1ebe856018465a7905a5b1f8ce6066f91ca
yamilrafart2 <yamilrafart2>
2025-08-14 19:00:18 -0300

    Add funcionalidades de calculo de area de un rectangulo

---
+++ Modulo-1/calculateArea/calculate_Area.js
@@ -0,0 +1,14 @@
+/**
+ * Definiendo variables y función para calcular el área
+ */
+let length;
+let width;
+
+function calculateArea() {
+    length = parseFloat(document.getElementById('length').value);
+    width = parseFloat(document.getElementById('width').value);
+
+    let area = length * width;
+
+     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+}
